886d5045c56d84e0402b96df3ad0b8a235863cf5
Ross J Hagan <rossjhagan@gmail.com>
2015-01-11 20:36:10 +0000

    Adds cachematrix for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix extends a basic matrix with the ability to cache solves.  
+## Using the setter and getter methods $set() and $get() will 
+## ensure that solves are only recalculated when the data has changed.
+##
+## Use in combination with cacheSolve(x, ...) instead
+## of solve(x, ...) to get the cached effect.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix encapsulates a matrix 'x' with caching functionality
+## If the inverse is not cached $getinverse will return NULL
+## Changes made through the $set method will reset the cache to NULL
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve receives a CacheMatrix and forwards any other
+## parameters to the standard solve(x, ...) function.  
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if ( !is.null(i) ) {
+    # debug: message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
